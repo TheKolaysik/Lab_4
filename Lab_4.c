9c15e803fdf052b957f0f372cd44ca806427b43d
Kolaysik <tolstyhnikolai@gmail.com>
2024-09-29 15:33:49 +0300

    задание 2 пункт 6

--- Lab_4.c
+++ Lab_4.c
@@ -7,11 +7,5 @@
 void main() {
 	setlocale(LC_ALL, "RUS");
 	int a = 11, b = 3;
-	int x;
-	float y;
-	double z;
-	x = a / b;
-	y = a / b;
-	z = a / b;
-	printf("%d\t%f\t%le\n", x, y, z);
+	printf("%d\t%f\t%le\n", (int)a/b , (float)a/b, (double)a/b);
 }
